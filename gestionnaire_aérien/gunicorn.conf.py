05d09d03b9676a889fae545705f325dd9264a343
richou <richou672005@protonmail.com>
2024-05-22 16:28:19 +0200

    deploy

--- gestionnaire_aérien/gunicorn.conf.py
+++ gestionnaire_aérien/gunicorn.conf.py
@@ -1,8 +1,8 @@
 command = '/usr/bin/gunicorn'
 pythonpath = '.'
-bind = '127.0.0.1:8000'
+bind = 'unix:/home/kali/gestionnaire_aérien/gestionnaire_aérien.sock'
 workers = 3
-user = 'username'
+user = 'kali'
 limit_request_fields = 32000
 limit_request_field_size = 0
 raw_env = 'DJANGO_SETTINGS_MODULE=gestionnaire_aérien.settings'
