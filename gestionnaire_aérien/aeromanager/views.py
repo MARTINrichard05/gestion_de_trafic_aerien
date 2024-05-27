9225c3e817496e8a508e5ff20a7568a4f6bb8bee
richou <richou672005@protonmail.com>
2024-05-27 21:09:55 +0200

    Grosse progression, les images devraient fonctionner en prod

--- gestionnaire_aérien/aeromanager/views.py
+++ gestionnaire_aérien/aeromanager/views.py
@@ -32,7 +32,7 @@ def aeroport_detail(request, id):
 
 def avion_detail(request, id):
     avion = get_object_or_404(Avions, id=id)
-    type_avion = get_object_or_404(TypesAvions, id=avion.type_avion.id)
+    type_avion = get_object_or_404(TypesAvions, id=avion.modele.id)
     context = {
         'avion': avion,
         'type_avion': type_avion,
